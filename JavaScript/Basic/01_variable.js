7ad5b5f6c5eda5a942c07c08467ae28ae8a90ae2
payalrathore0603 <75780396+payalrathore0603@users.noreply.github.com>
2024-05-21 18:23:55 +0000

    variable and datatype

---
+++ JavaScript/Basic/01_variable.js
@@ -0,0 +1,13 @@
+const userId=1234;
+let userName="Payal";
+var userEmail="payal@gmail.com" //var perfer not to use due issue of block scope and funtion scope
+
+userPassword="payal" //not to use
+
+let userAddress; //undefine
+//const userAddress2; // const declaration must be initialized 
+
+console.log(userEmail)
+console.table([userId,userName,userEmail,userPassword,userAddress])
+
+
---
+++ JavaScript/Basic/02_datatyps.js
@@ -0,0 +1,20 @@
+"use strict" // That will allow to allow new versio rule
+
+let name="payal" //String
+
+/*
+Data Type:
+1. number : range 2^53
+2. bigInt
+3. string
+4. boolean : True/False
+5. Symbol : unique (used in react which gives you unquie value)
+6. null  : standalomne value (Empty)
+7. undife : not intialize 
+
+1. object
+*/
+
+console.log(typeof name);
+console.log(typeof null); // object
+console.log(typeof undefined); //undefine

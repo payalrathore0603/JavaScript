a90e91d5364fed4394db23a32078f8d0060c1157
payalrathore0603 <75780396+payalrathore0603@users.noreply.github.com>
2024-05-22 17:17:25 +0000

    stack and heap

--- JavaScript/Basic/04_basicOfDatatypes.js
+++ JavaScript/Basic/04_basicOfDatatypes.js
@@ -26,7 +26,7 @@ console.log(typeof temprature)  // null typeof Object
 console.log(typeof randomNum)  // undefine
 */
 
-// Non-Primitives
+// Non-Primitives (Reference)
 // Array, object, functons
 
 let juice=["lasi", "mango", "masumbhi", "pinapples"]
@@ -47,10 +47,51 @@ let menu={
 
 }
 
+function myfun(){
+    //stmt
+}
+
+/*
 // All these are function Object
 console.log(typeof juice)
 console.log(typeof menu);
 console.log(typeof menu.item1);
 // this is a string
 console.log(typeof menu.item1.name);
+console.log(typeof myfun)  // function
+
+*/
+
+// +++++++++++++++++++++++++++++ Stack and heap +++++++++++++++++++++++++++++++
+
+// shake and jucie are store there value in stack
+// shake value COPY into juice
+let shake="mango"
+let juice1=shake;
+
+
+// menu1 value store in heap where as in stack they store refernce of heap where value store
+let menu1={
+    item1:{
+        name:"mango",
+        price:120
+    },
+    item2:{
+        name:"lasi",
+        price:160
+    },
+    item3:{
+        name:"masumbhi",
+        price:100
+    }
+
+}
+
+let newMenu=menu1;
+
+
+menu1.item1.name="Orange"
+console.log(menu1)  // value update in both the palce because they store refernce 
+console.log(newMenu)
+
 

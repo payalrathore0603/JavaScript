8ff2b48244bff9e0beeead743cfb87e78591dcd3
payalrathore0603 <75780396+payalrathore0603@users.noreply.github.com>
2024-06-09 11:11:36 +0000

    DOM get Element

---
+++ JavaScript/Intermidate/01.DOM.js
@@ -0,0 +1,25 @@
+//------------------- Widow : Global document
+// console.log(window)
+// console.log(document);
+// document.baseURI
+// document.links
+// document.links[0]
+
+// document.getElementById('fnid').innerHTML="<p>Payal</p>"
+
+// getElementById... className, getAttribte, setAttribute(1 arg must)
+
+
+// HTLMCOLLEction(convert it into Array.form(arrName)), NodeCollection(foreach): are not have array function
+// check proptotype
+
+
+//TextContext:show hidden content as well {Display: "hidden"}
+//InnerText: Not show hidden content
+//InnerHTML: Show HTML tag as well
+
+
+//==========================  QuerySelector and QureySelectorALL (retrun HTML Collection) =================
+
+
+

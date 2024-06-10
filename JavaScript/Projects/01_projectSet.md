dafff316d7247d816fc2aab4df67db1383524836
payalrathore0603 <75780396+payalrathore0603@users.noreply.github.com>
2024-06-10 00:18:23 +0000

    Project color switch made by me

---
+++ JavaScript/Projects/01_projectSet.md
@@ -0,0 +1,5 @@
+# Project related to DOM
+
+## Project Link
+
+[Click Here](https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html)
---
+++ JavaScript/Projects/colorSwitch.js
@@ -0,0 +1,26 @@
+{/* <button class="box" id="gray"></button> */}
+
+
+function grayClick(){
+    const bgChnage=document.querySelector('body')
+    bgChnage.style.backgroundColor="gray"
+    bgChnage.style.color="white"
+}
+
+function whiteClick(){
+        const bgChnage=document.querySelector('body')
+    bgChnage.style.backgroundColor=" rgb(230, 230, 198)"
+    bgChnage.style.color="black"
+}
+
+function blueClick(){
+        const bgChnage=document.querySelector('body')
+    bgChnage.style.backgroundColor="blue"
+    bgChnage.style.color="white"
+}
+
+function yellowClick(){
+    const bgChnage=document.querySelector('body')
+bgChnage.style.backgroundColor="yellow"
+bgChnage.style.color="black"
+}
